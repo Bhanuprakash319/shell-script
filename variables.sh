2aa049ac85a1fb8b36c5f87b522b7234a395ee3d
Bhanu prakash reddy <bhanu@Bhanus-Laptop.lan>
2025-03-21 13:28:04 +0530

    variables-desc

--- variables.sh
+++ variables.sh
@@ -4,7 +4,10 @@ Institution=$2
 
 echo "Hi, I am $Name, age $Age, studying at $Institution"
 
-fav_movie=$3
-fav_sport=$4
+echo "enter your favorite movie"
+read fav_movie
+
+echo "enter your favorite sport"
+read fav_sport
 
 echo "My favorite movie is $fav_movie and favorite sport is $fav_sport"
