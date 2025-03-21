5271cd2083578c1b05cb62f12caa02570f515f39
Bhanu prakash reddy <bhanu@Bhanus-Laptop.lan>
2025-03-21 14:22:20 +0530

    variables-desc

--- arrays.sh
+++ arrays.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+#arrays declaration
 Movies=("salaar" "kalki" "spirit")
 
 echo "first movie is ${Movies[0]}"
 echo "Movies are ${Movies[@]}"
+
+#sum of three numbers
+echo "enter the three numberes to be added"
+read num1
+read num2
+read num3
+
+sum=$((num1+num2+num3))
+echo "sum of the entered three numbers is $sum"
