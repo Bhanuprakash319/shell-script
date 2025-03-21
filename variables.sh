97ef72dacf695212bcea5ecd86277514940e69e9
Bhanu prakash reddy <bhanu@Bhanus-Laptop.lan>
2025-03-21 14:07:23 +0530

    variables-desc

--- variables.sh
+++ variables.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
-
-# Assigning values
-Name="Bhanu"
+Name=Bhanu
 Age=$1
 Institution=$2
 
-# Displaying initial details
-echo "Hi, I am $Name, age $Age, studying at $Institution."
+echo "Hi, I am $Name, age $Age, studying at $Institution"
 
-# Taking user input
-echo "Enter your favorite movie:"
+echo "enter your favorite movie"
 read fav_movie
 
-echo "Enter your favorite sport:"
+echo "enter your favorite sport"
 read fav_sport
 
-# Displaying user input
-echo "My favorite movie is $fav_movie and my favorite sport is $fav_sport."
-
+echo "My favorite movie is $fav_movie and favorite sport is $fav_sport"
