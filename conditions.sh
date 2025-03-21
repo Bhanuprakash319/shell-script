00b7e12973c1687c52183121ea4b92fec79d5685
Bhanu prakash reddy <bhanu@Bhanus-Laptop.lan>
2025-03-21 14:44:47 +0530

    conditions&special_var-desc

--- conditions.sh
+++ conditions.sh
@@ -10,3 +10,18 @@ then
 else
     echo "You are not eligible for a driving license"
 fi
+
+#special variables code
+echo "the variables are $@"
+
+echo "total variables are $#"
+
+echo "script name is $0"
+
+echo "current working directory is $PWD"
+
+echo "home directory of current user is $HOME"
+
+echo "current which user is running this script is $USER"
+
+echo "Hostname of the server is $HOSTNAME"
