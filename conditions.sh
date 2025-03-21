5191e51fb9a2c09a01a450003c3f54227b91ec5e
Bhanu prakash reddy <bhanu@Bhanus-Laptop.lan>
2025-03-21 14:38:34 +0530

    variables-desc

--- conditions.sh
+++ conditions.sh
@@ -4,7 +4,7 @@ echo "This program is to check the eligibility of a driving license"
 echo "Enter your age"
 read age
 
-if [$age -ge 18]
+if [ $age -ge 18 ]
 then 
     echo "You are eligible for driving license"
 else
